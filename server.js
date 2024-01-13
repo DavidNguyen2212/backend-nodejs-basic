3331efa12f6600f7cfd2d33b603c697c4a38a728
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-01-13 15:43:29 +0700

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const http = require("node:http");
-const hostname = "127.0.0.1";
+const http = require("node:http"); // có sẵn trong node js
+const hostname = "127.0.0.1"; // local host ip hoặc dùng 'localhost'
 const port = 3000;
+
+// Tạo server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World CCCCC\n");
 });
+
+// Cho server chạy ở đâu + callback function
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
